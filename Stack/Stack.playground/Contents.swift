1a4881dfbd58f875668438690a5b7991560368a2
Mikael <miketekle14@gmail.com>
2016-11-13 01:37:06 -0800

    Created Stack data structure in swift playground

---
+++ Stack/Stack.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+public struct Stack<T> {
+    
+    fileprivate var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count : Int {
+        return array.count
+    }
+    
+    public mutating func push(element: T) {
+        array.append(element)
+    }
+    
+    public mutating func pop(_ element: T) -> T? {
+        return array.popLast()
+    }
+    
+    public mutating func peek() -> T? {
+        return array.last
+    }
+    
+}
+
+
+var someStack = Stack(array: [])
+someStack.push(element: "Mikael")
+someStack.push(element: "Alem")

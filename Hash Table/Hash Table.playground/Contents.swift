0dc17bad11527b5c90f53d46f1c3ae7006e3db04
Mikael <miketekle14@gmail.com>
2016-11-19 14:07:39 -0800

    created class for hashtable with variables and initializers, created Todos

--- Hash Table/Hash Table.playground/Contents.swift
+++ Hash Table/Hash Table.playground/Contents.swift
@@ -1,2 +1,32 @@
 import Foundation
 
+public struct HashTable<Key: Hashable, Value> {
+    
+    fileprivate typealias Element = (key: Key, value: Value)
+    fileprivate typealias Bucket = [Element]
+    private var buckets : [Bucket]
+    
+    private(set) var count = 0
+    
+    public init (capacity: Int) {
+        
+        assert(capacity > 0)
+        buckets = Array<Bucket>(repeatElement([], count: capacity))
+    }
+}
+
+var hashTable1 = HashTable<String, String>(capacity: 20)
+//hashTable1.buckets
+
+//==================================
+//insert
+//hashTable1["firstName"] = "Mikael"
+
+//update
+//hashTable1["firstName"] = "Kidan"
+
+//delete
+//hashTable1["firstName"] = nil
+
+//lookup 
+//let me = hashTable1["firstName"]

ec3ef3a93bbff68bc98302919087cf2ea00e09a7
Mikael <miketekle14@gmail.com>
2016-11-19 14:55:09 -0800

    created functionality for hash table to insert and update values, retrieve index of key in chain, look up value given hash table key

--- Hash Table/Hash Table.playground/Contents.swift
+++ Hash Table/Hash Table.playground/Contents.swift
@@ -4,14 +4,18 @@ public struct HashTable<Key: Hashable, Value> {
     
     fileprivate typealias Element = (key: Key, value: Value)
     fileprivate typealias Bucket = [Element]
-    private var buckets : [Bucket]
+    fileprivate var buckets : [Bucket]
     
-    private(set) var count = 0
+    fileprivate(set) var count = 0
     
     public init (capacity: Int) {
         
         assert(capacity > 0)
         buckets = Array<Bucket>(repeatElement([], count: capacity))
+    }
+    
+    fileprivate func index(forKey key: Key) -> Int {
+        return abs(key.hashValue) % buckets.count
     }
 }
 
@@ -19,14 +23,67 @@ var hashTable1 = HashTable<String, String>(capacity: 20)
 //hashTable1.buckets
 
 //==================================
-//insert
+//TODO: insert
 //hashTable1["firstName"] = "Mikael"
 
-//update
+//TODO: update
 //hashTable1["firstName"] = "Kidan"
 
-//delete
+//TODO: delete
 //hashTable1["firstName"] = nil
 
-//lookup 
+//TODO: lookup
 //let me = hashTable1["firstName"]
+
+extension HashTable {
+    
+    //lookup
+    func value(forKey key: Key) -> Value? {
+        let index = self.index(forKey: key)
+        for element in buckets[index] {
+            if element.key == key {
+                return element.value
+            }
+        }
+        return nil
+    }
+    
+    //update, insert
+    public mutating func updateValue(_ value: Value, forKey key: Key) -> Value? {
+        //loop through chain and check for value, if found update
+        let index = self.index(forKey: key)
+        
+        for (i, element) in buckets[index].enumerated() {
+            if element.key == key {
+                let oldValue = element.value
+                buckets[index][i].value = value
+                return oldValue
+            }
+        }
+        
+        buckets[index].append((key: key, value: value))
+        count += 1
+        return nil
+    }
+    
+    //remove 
+    public mutating func removeValue(forKey key: Key) -> Value? {
+        //loop through hashtable chains, if value is found, delete
+        
+        return nil
+    }
+    
+    
+    public subscript(key: Key) -> Value? {
+        
+        get {
+            return value(forKey: key)
+        }
+        
+        set {
+            
+        }
+        
+    }
+    
+}

397c97393cad8aeac0fd4923dddc044aa89e4d4b
Mikael <miketekle14@gmail.com>
2016-11-19 16:24:51 -0800

    created method for traversing binary tree in order

--- Trees/BinaryTree.playground/Contents.swift
+++ Trees/BinaryTree.playground/Contents.swift
@@ -3,10 +3,37 @@ import Foundation
 indirect public enum BinaryTree<T> {
     case Node(BinaryTree<T>, T, BinaryTree<T>)
     case Empty
+    
+}
+
+extension BinaryTree {
+    public var count : Int {
+        switch self {
+        case let .Node(left, _, right):
+            return left.count + 1 + right.count
+        case .Empty:
+            return 0
+        }
+    }
+    
+    //TODO: In-Order (depth first) Search
+    func traverseInOrder(process: (T) -> Void) {
+        if case let .Node(left, value, right) = self {
+            left.traverseInOrder(process: process)
+            process(value)
+            right.traverseInOrder(process: process)
+        }
+    }
+    
+    //TODO: PreOrder Search
+    
+    
+    //TODO: PostOrder Search
+    
 }
 
 let node2 = BinaryTree.Node(.Empty, 2, .Empty)
-let node12 = BinaryTree.Node(.Empty, 12, .Empty)
+let node12 = BinaryTree.Node(node2, 12, .Empty)
 let node1 = BinaryTree.Node(.Empty, 1, .Empty)
 let node3 = BinaryTree.Node(.Empty, 3, .Empty)
 let node11 = BinaryTree.Node(node3, 11, .Empty)
@@ -15,4 +42,16 @@ let node9 = BinaryTree.Node(.Empty, 9, .Empty)
 let node5 = BinaryTree.Node(node9, 5, node7)
 let node4 = BinaryTree.Node(.Empty, 4, node11)
 let node8 = BinaryTree.Node(node5, 8, node4)
+
+
+
+node8.traverseInOrder { (int) in
+    print(int)
+}
+
+
+
+
+
+
 

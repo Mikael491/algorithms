1dd72e657dab7d41b4194024f3dff074160738b6
Mikael <miketekle14@gmail.com>
2017-01-31 10:20:12 -0800

    added helper variables for calling head and tail nodes

--- problems/LinkedLists.playground/Contents.swift
+++ problems/LinkedLists.playground/Contents.swift
@@ -1,9 +1,10 @@
 import Foundation
 
+
 class ListNode<T> {
     var value: T
     var next: ListNode?
-    weak var previous: ListNode?
+    var prev: ListNode?
     init(value: T) {
         self.value = value
     }
@@ -12,4 +13,21 @@ class ListNode<T> {
 class LinkedList<T> {
     public typealias Node = ListNode<T>
     private var head : Node?
+    private var tail : Node?
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var first : Node? {
+        return head
+    }
+    
+    public var last : Node? {
+        return tail
+    }
 }
+
+var myList = LinkedList<String>()
+
+

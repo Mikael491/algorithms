c61b21c23d100fad4ca1589921f7f467794eb3bd
Mikael <miketekle14@gmail.com>
2017-01-30 17:27:19 -0800

    added problem 1 solution

--- problems/ArraysAndStrings.playground/Contents.swift
+++ problems/ArraysAndStrings.playground/Contents.swift
@@ -1,3 +1,17 @@
 import Foundation
 
+//Problem 1.1
+func hasUniqueCharacters(string: String) -> Bool {
+    
+    var unique = Set<Character>()
+    
+    for x in string.characters {
+        guard !unique.contains(x) else { return false }
+        unique.insert(x)
+    }
+    
+    return true
+}
 
+let name = "mikael"
+hasUniqueCharacters(string: name)
